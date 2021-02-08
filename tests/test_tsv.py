bc2a71eac435813c87a23033116c4f1e55355e3e
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-02-07 18:01:08 -0800

    fixed default thread safe variable

--- tests/test_tsv.py
+++ tests/test_tsv.py
@@ -8,22 +8,24 @@ from threadsafevariable import ThreadSafeVariable
 
 class A(object):
     x = ThreadSafeVariable()
+
     def __init__(self, x):
         self.x = x
         ThreadSafeVariable._image = ThreadSafeVariable.freeze()
+
     def restore(self):
         ThreadSafeVariable.restore(ThreadSafeVariable._image)
-        
+
 
 class MyThread(threading.Thread):
     def run(self):
+        assert self.a.x == 3
         self.a.x = 5
-        time.sleep(.2)
+        time.sleep(0.2)
         assert self.a.x == 5
 
 
 class MyTest(unittest.TestCase):
-
     def test_tsv(self):
         a = A(4)
         b = A(2)
@@ -31,15 +33,15 @@ class MyTest(unittest.TestCase):
         t = MyThread()
         t.a = a
         t.start()
-        time.sleep(.3)
+        time.sleep(0.3)
         assert a.x == 3
         t.join()
         assert b.x == 2
         a.restore()
         assert a.x == 4
-        a.x = 1
+        a.x = 3
         t = MyThread()
         t.a = a
         t.start()
-        time.sleep(.3)
-        assert a.x == 1
+        time.sleep(0.3)
+        assert a.x == 3
--- threadsafevariable/__init__.py
+++ threadsafevariable/__init__.py
@@ -2,18 +2,26 @@ import copy
 import threading
 
 
-__version__ = 1.0
+__version__ = 1.1
 
 
-class ThreadSafeVariable(object):
+class ThreadSafeVariable:
 
     storage = threading.local()
+    master_thread = threading.current_thread()
+    defaults = {}
 
     def __set__(self, instance, value):
+        if self.master_thread == threading.current_thread():
+            self.defaults[id(instance)] = value
         setattr(self.storage, "%s.%s" % (id(instance), id(self)), value)
 
     def __get__(self, instance, owner):
-        return getattr(self.storage, "%s.%s" % (id(instance), id(self)))
+        return getattr(
+            self.storage,
+            "%s.%s" % (id(instance), id(self)),
+            self.defaults.get(id(instance)),
+        )
 
     @staticmethod
     def freeze():
