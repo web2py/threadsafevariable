a8b07ffb480bd38496d1640dcf03dd1a67891d19
laund <me@laund.moe>
2025-07-16 18:21:09 +0200

    Fix hot-reload crash

--- threadsafevariable/__init__.py
+++ threadsafevariable/__init__.py
@@ -11,15 +11,22 @@ class ThreadSafeVariable:
     storage.vars = {}
     master_thread = threading.current_thread()
     defaults = {}
+    
+    @staticmethod
+    def ensure_vars():
+        if getattr(ThreadSafeVariable.storage, "vars", None) is None:
+            ThreadSafeVariable.storage.vars = {}
 
     def __set__(self, instance, value):
         key = "%s.%s" % (id(instance), id(self))
         if self.master_thread == threading.current_thread():
             self.defaults[key] = value
+        self.ensure_vars()
         self.storage.vars[key] = value
 
     def __get__(self, instance, owner):
         key = "%s.%s" % (id(instance), id(self))
+        self.ensure_vars()
         try:
             return self.storage.vars[key]
         except KeyError: 
@@ -27,8 +34,10 @@ class ThreadSafeVariable:
 
     @staticmethod
     def freeze():
+        ThreadSafeVariable.ensure_vars()
         return copy.copy(ThreadSafeVariable.storage.vars)
 
     @staticmethod
     def restore(image):
+        ThreadSafeVariable.ensure_vars()
         ThreadSafeVariable.storage.vars = copy.copy(image)
